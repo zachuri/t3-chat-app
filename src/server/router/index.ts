f92a81da8c9dca230f7f7f592005f976e31c7d2a
zachuri <punsalang.zachary@gmail.com>
2022-08-26 22:10:21 -0700

    setup: roomRouter -> w/ index.ts & room.ts

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,13 +2,12 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
+import { roomRouter } from "./room";
 import { protectedExampleRouter } from "./protected-example-router";
 
 export const appRouter = createRouter()
-  .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", protectedExampleRouter);
+	.transformer(superjson)
+	.merge("room.", roomRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
