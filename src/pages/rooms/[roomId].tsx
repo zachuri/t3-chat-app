e03d91f5765524cca4a96607895d93953287f60c
zachuri <punsalang.zachary@gmail.com>
2022-08-27 00:41:33 -0700

    created/fix: added login and check user session/ fixed why login isn't working (works now)

--- src/pages/rooms/[roomId].tsx
+++ src/pages/rooms/[roomId].tsx
@@ -1,9 +1,22 @@
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
 function RoomPage() {
 	const { query } = useRouter();
 
 	const roomId = query.roomId as string;
+
+	// make sure user is logged in
+	const { data: session } = useSession();
+
+	// if user not signed in
+	if (!session) {
+		return (
+			<div>
+				<button onClick={() => signIn()}>Login</button>
+			</div>
+		);
+	}
 
 	return <div>welcome to room {roomId}</div>;
 }
