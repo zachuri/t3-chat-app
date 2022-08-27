84e68e8d86b931886660bbbb685418a1df7657ff
zachuri <punsalang.zachary@gmail.com>
2022-08-26 23:47:42 -0700

    fixed: webSocketServer working

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,18 +1,23 @@
 // src/server/db/client.ts
 import { PrismaClient } from "@prisma/client";
-import { env } from "../../env/server.mjs";
+import dynamic from "next/dynamic.js";
+// import { env } from "../../env/server.mjs";
+
+// Solved WebSocket Server issue
+// - need to dynamically import
+const env = dynamic(() => import("../../env/server.mjs"));
 
 declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+	// eslint-disable-next-line no-var
+	var prisma: PrismaClient | undefined;
 }
 
 export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
+	global.prisma ||
+	new PrismaClient({
+		log: ["query"],
+	});
 
 if (env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+	global.prisma = prisma;
 }
