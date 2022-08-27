4f77bdb7ceff21ef1453850274728423d51723d1
zachuri <punsalang.zachary@gmail.com>
2022-08-26 21:51:12 -0700

    _app.tsx: setup config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,43 +5,68 @@ import type { AppType } from "next/dist/shared/lib/utils";
 import superjson from "superjson";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+import { wsLink, createWSClient } from "@trpc/client/links/wsLink";
 
 const MyApp: AppType = ({
-  Component,
-  pageProps: { session, ...pageProps },
+	Component,
+	pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+	return (
+		<SessionProvider session={session}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 };
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+	if (typeof window !== "undefined") return ""; // browser should use relative url
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+	return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
-export default withTRPC<AppRouter>({
-  config() {
-    /**
-     * If you want to use SSR, you need to use the server's full URL
-     * @link https://trpc.io/docs/ssr
-     */
-    const url = `${getBaseUrl()}/api/trpc`;
+const url = `${getBaseUrl()}/api/trpc`;
 
-    return {
-      url,
-      transformer: superjson,
-      /**
-       * @link https://react-query.tanstack.com/reference/QueryClient
-       */
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
-    };
-  },
-  /**
-   * @link https://trpc.io/docs/ssr
-   */
-  ssr: false,
+function getEndingLink() {
+	if (typeof window === "undefined") {
+		return httpBatchLink({
+			url,
+		});
+	}
+
+	const client = createWSClient({
+		url: process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:3011",
+	});
+
+	return wsLink({
+		client,
+	});
+}
+
+export default withTRPC<AppRouter>({
+	config({ ctx }) {
+		/**
+		 * If you want to use SSR, you need to use the server's full URL
+		 * @link https://trpc.io/docs/ssr
+		 */
+
+		return {
+			links: [getEndingLink()],
+			transformer: superjson,
+			/**
+			 * @link https://react-query.tanstack.com/reference/QueryClient
+			 */
+			// queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+			headers() {
+				if (ctx?.req) {
+					return { ...ctx.req.headers };
+				}
+				return {};
+			},
+		};
+	},
+	/**
+	 * @link https://trpc.io/docs/ssr
+	 */
+	ssr: false,
 })(MyApp);
