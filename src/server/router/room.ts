c61b13703f04e8f2bd454de77428bcd23a87b35a
zachuri <punsalang.zachary@gmail.com>
2022-08-27 00:07:24 -0700

    roomRouter: added mutation(send-message) getting user input

--- src/server/router/room.ts
+++ src/server/router/room.ts
@@ -1,4 +1,34 @@
 import { createRouter } from "./context";
-import { z } from "zod";
+import {
+	Message,
+	messageSubSchema,
+	sendMessageSchema,
+} from "../../constants/schemas";
+import { Events } from "../../constants/events";
+import { randomUUID } from "crypto";
 
 export const roomRouter = createRouter()
+	.mutation("send-message", {
+		input: sendMessageSchema,
+		resolve({ ctx, input }) {
+			const message: Message = {
+				id: randomUUID(),
+				...input,
+				sentAt: new Date(),
+				sender: {
+					name: ctx.session?.user?.name || "unknown",
+				},
+				// roomId: ""
+			};
+
+			ctx.ee.emit(Events.SEND_MESSAGE, message);
+
+			return true;
+		},
+	})
+	.subscription("onSendMessage", {
+		input: messageSubSchema,
+		resolve() {
+			return true;
+		},
+	});
