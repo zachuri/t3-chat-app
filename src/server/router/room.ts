b5b771fe5b7a952cd6311cb8200f00e6abdd348a
zachuri <punsalang.zachary@gmail.com>
2022-08-27 00:12:24 -0700

    setup roomServer: about completed server side

--- src/server/router/room.ts
+++ src/server/router/room.ts
@@ -6,6 +6,8 @@ import {
 } from "../../constants/schemas";
 import { Events } from "../../constants/events";
 import { randomUUID } from "crypto";
+import * as trpc from "@trpc/server";
+import { createNextApiHandler } from "@trpc/server/adapters/next";
 
 export const roomRouter = createRouter()
 	.mutation("send-message", {
@@ -28,7 +30,18 @@ export const roomRouter = createRouter()
 	})
 	.subscription("onSendMessage", {
 		input: messageSubSchema,
-		resolve() {
-			return true;
+		resolve({ctx, input}) {
+			return new trpc.Subscription((emit) => {
+        function onMessage(data: Message){
+          if (input.roomId === data.roomId) {
+            emit.data(data)
+          }
+        }
+        ctx.ee.on(Events.SEND_MESSAGE, onMessage)
+
+        return () => {
+          ctx.ee.off(Events.SEND_MESSAGE, onMessage);
+        }
+      });
 		},
 	});
