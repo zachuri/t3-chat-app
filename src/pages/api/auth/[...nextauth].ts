ffa91872ad87700337c36581f6d6817c0da4ffaf
zachuri <punsalang.zachary@gmail.com>
2022-08-26 21:48:25 -0700

    next-auth: setup next authentication

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+// import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -7,24 +8,42 @@ import { prisma } from "../../../server/db/client";
 import { env } from "../../../env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
+	// Include user.id on session
+	callbacks: {
+		session({ session, user }) {
+			if (session.user) {
+				session.user.id = user.id;
+			}
+			return session;
+		},
+	},
+	// Configure one or more authentication providers
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		// DiscordProvider({
+		//   clientId: env.DISCORD_CLIENT_ID,
+		//   clientSecret: env.DISCORD_CLIENT_SECRET,
+		// }),
+		// ...add more providers here
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				name: {
+					label: "Name",
+					type: "text",
+					placeholder: "Enter your name",
+				},
+			},
+			async authorize(credentials, _req) {
+				const user = { id: 1, name: credentials?.name ?? "J Smith" };
+				return user;
+			},
+		}),
+	],
+	secret: process.env.NEXTAUTH_SECRET,
+	session: {
+		strategy: "jwt",
+	},
 };
 
 export default NextAuth(authOptions);
